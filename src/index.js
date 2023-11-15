656bac58103efdf2f01cda7df0f29616a928fb45
Gulista <gulistasiddiqui5@gmail.com>
2023-11-16 00:05:46 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import Login from './components/Login';
+//import TableBook from './components/TableBook';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+ <App />,
+     document.getElementById('root'));
+
